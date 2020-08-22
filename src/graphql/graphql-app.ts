36c2ad4e202ee4994494b04102ec110a07585b17
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-08-23 03:38:17 +0430

    🎨 Improve user registration and fix bugs

--- src/graphql/graphql-app.ts
+++ src/graphql/graphql-app.ts
@@ -1,25 +1,69 @@
 import 'reflect-metadata';
 import debug from 'debug';
 import mongoose from 'mongoose';
-import { ApolloServer } from 'apollo-server';
+import Express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import session from 'express-session';
+import connectMongo from 'connect-mongo';
+
 import { DeviceResolver } from './resolvers/device-resolver';
 import { UserResolver } from './resolvers/user-resolver';
 import { formatError } from '../errors';
 
 mongoose.connection.on('connected', async () => {
+  const dev = process.env.NODE_ENV !== 'production';
   const log = debug('Graphql:App');
+
   log('Starting Graphql server...');
 
   const schema = await buildSchema({
     resolvers: [DeviceResolver, UserResolver],
   });
+
   const server = new ApolloServer({
     schema,
     formatError,
+    debug: true,
+    context: ({ req }: any) => ({ req }),
   });
 
-  server.listen().then(({ url }) => {
-    log(`🚀  Server ready at ${url}`);
+  const app = Express();
+
+  const corsOptions = {
+    credentials: true,
+    origin: process.env.CORS_ORIGIN!,
+  };
+
+  const MongoStore = connectMongo(session);
+  app.use(
+    session({
+      store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 1000 * 60 * 60 * 24, // 1 Day
+      }),
+      name: 'qid',
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: !dev,
+        maxAge: 1000 * 60 * 60 * 24, // 1 Day
+      },
+    }),
+  );
+
+  if (!dev) {
+    app.set('trust proxy', 1); // trust first proxy
+  }
+
+  server.applyMiddleware({ app, cors: corsOptions });
+
+  const port = parseInt(process.env.GRAPHQL_PORT!, 10);
+  app.listen(port, (err) => {
+    if (err) throw new Error(err);
+
+    log(`🚀  Server ready at http://localhost:${port}/graphql`);
   });
 });
--- src/graphql/resolvers/user-resolver.ts
+++ src/graphql/resolvers/user-resolver.ts
@@ -1,29 +1,97 @@
-import { Resolver, Mutation, Arg, Query } from 'type-graphql';
-import { UserInput } from '../types/user-types';
+import { Resolver, Mutation, Arg, Query, Ctx, UseMiddleware, ArgumentValidationError } from 'type-graphql';
+import bcrypt from 'bcrypt';
+import _ from 'underscore';
+
+import { CreateUserInput, UpdateUserInput, LoginInput } from '../types/user-types';
 import { User, UserModel } from '../../models/user';
+import { MyContext } from '../../@types/access-control';
+import { isAuth } from '../middlewares/isAuth';
+import { UserInputError } from 'apollo-server-express';
 
 @Resolver()
 export class UserResolver {
-  @Query(() => User, { nullable: false })
-  async singleUser(@Arg('id') id: string): Promise<User | null> {
-    return await UserModel.findById({ _id: id });
+  @Query(() => User, { nullable: true })
+  @UseMiddleware(isAuth)
+  async me(@Ctx() ctx: MyContext): Promise<User | null> {
+    const { userId } = ctx.req.session!;
+
+    if (!userId) return null;
+
+    return await UserModel.findById(userId);
   }
 
-  @Query(() => [User])
-  async allUsers(): Promise<User[]> {
-    return await UserModel.find().populate({ path: 'devices', model: 'Device' });
+  @Query(() => Boolean)
+  async checkAuth(@Ctx() ctx: MyContext): Promise<Boolean> {
+    const { userId } = ctx.req.session!;
+    return !!userId;
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async login(@Arg('user') { email, password }: LoginInput, @Ctx() ctx: MyContext): Promise<User | null> {
+    const user = await UserModel.findOne({ email: email.toLowerCase() });
+
+    const error = new ArgumentValidationError([
+      {
+        children: [],
+        property: 'inputs',
+        constraints: { inputs: 'Invalid email or password!' },
+      },
+    ]);
+
+    if (!user) {
+      throw error;
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw error;
+    }
+
+    ctx.req.session!.userId = user._id;
+
+    return user;
   }
 
   @Mutation(() => User)
-  async addUser(@Arg('user') { username, password }: UserInput): Promise<User> {
-    const newUser = (await UserModel.create({ username, password })).save();
+  async register(@Arg('user') { email, password, firstName, lastName }: CreateUserInput): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await UserModel.create({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
+    await newUser.save();
 
     return newUser;
   }
 
   @Mutation(() => Boolean)
-  async removeUser(@Arg('id') id: string): Promise<boolean> {
-    await UserModel.deleteOne({ id });
-    return true;
+  @UseMiddleware(isAuth)
+  async logout(@Ctx() ctx: MyContext): Promise<Boolean> {
+    return new Promise((resolve, reject) => {
+      ctx.req.session!.destroy((err) => {
+        if (err) {
+          reject(false);
+        }
+
+        resolve(true);
+      });
+    });
+  }
+
+  @Mutation(() => User)
+  @UseMiddleware(isAuth)
+  async updateMe(@Arg('user') { firstName, lastName }: UpdateUserInput, @Ctx() ctx: MyContext): Promise<User | null> {
+    const user = await UserModel.findOneAndUpdate(
+      { _id: ctx.req.session!.userId },
+      {
+        $set: _.pick({ firstName, lastName }, _.isString),
+      },
+    );
+
+    return user;
   }
 }
